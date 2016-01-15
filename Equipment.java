a9b43fd8fde4ce0ef02b5f789b0f15f46685f8e2
getDolla <ywang8@stuy.edu>
2016-01-15 04:56:32 -0500

    Equipment added

---
+++ Equipment.java
@@ -0,0 +1,21 @@
+public class Equipment extends Items {
+    protected int boost;
+    public Equipment( String s, int p ) {
+	super( name );
+	boost = 0;
+    }
+
+    public int equip(Character c) {
+	
+	return boost;
+    }
+
+
+
+
+
+
+
+
+
+}
