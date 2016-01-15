b50c5107da8d43943cff3a90519296b390c702a9
getDolla <ywang8@stuy.edu>
2016-01-15 04:37:21 -0500

    modified Item

--- Item.java
+++ Item.java
@@ -1,2 +1,14 @@
 public class Item {
+    public String name;
+    public int price;
+
+    public Item( String s, int p ) { //mostly for money
+	name = s;
+	price = p;
+    }
+
+    public String toString() {
+	return name;
+    }
+
 }
