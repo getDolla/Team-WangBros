5d97c66d4ede8c906dc77da0c43c5345a1accc1c
getDolla <ywang8@stuy.edu>
2016-01-16 23:35:23 -0500

    added use and pickup

--- Character.java
+++ Character.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Character {
     public String appearance,name;
     protected int hp, speed, money, damage, luck;
@@ -5,6 +7,11 @@ public class Character {
     protected int[] location = { 1, 1 }; //initial start point
     public Floor tileUnder = new Floor();
 
+    public ArrayList armors = new ArrayList<Armor>();
+    public ArrayList sticks = new ArrayList<Weapon>();
+    public ArrayList adren = new ArrayList<Adrenaline>();
+    public ArrayList healthdrinks = new ArrayList<HpPotion>();
+    
     public Character() {
 	appearance = "*";
 	name = "Bob";
@@ -96,6 +103,67 @@ public class Character {
         speed = normalstats[1];
         damage = normalstats[2];
         luck = normalstats[3];
+    }
+
+    public void pickup( Item i ) {
+        if ( i instanceof Weapon ) {
+            sticks.add( (Weapon) i );
+        }
+
+        else if ( i instanceof Armor ) {
+            armors.add( (Armor) i );
+        }
+
+        else if ( i instanceof Adrenaline ) {
+            adren.add( (Adrenaline) i );
+        }
+
+        else if ( i instanceof HpPotion ) {
+            healthdrinks.add( (HpPotion) i );
+        }
+
+        else {
+            money += i.price;
+        }
+    }
+
+    public void use( Item i ) {
+         if ( i instanceof Weapon ) {
+            if ( i instanceof Twig ) {
+                Twig w = (Twig) i;
+            }
+
+            else {
+                Sword w = (Sword) i;
+            }
+
+            w.getBuff(this);
+        }
+
+        else if ( i instanceof Armor ) {
+            if ( i instanceof Light ) {
+                Light a = (Light) i;
+            }
+
+            else if ( i instanceof Medium ) {
+                Medium a = (Medium) i;
+            }
+
+            else {
+                Heavy a = (Heavy) i;
+            }
+                a.setCharstat( this );
+        }
+
+        else if ( i instanceof Adrenaline ) {
+            ((Adrenaline) i).drink( this );
+            adren.remove( (Adrenaline) i );
+        }
+
+        else if ( i instanceof HpPotion ) {
+            ((HpPotion) i).drink( this );
+            healthdrinks.remove( (HpPotion) i );
+        } 
     }
 
 }
