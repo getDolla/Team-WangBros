36fb0c5735b95047af01420847cc60f484116fcd
getDolla <ywang8@stuy.edu>
2016-01-18 00:09:11 -0500

    Modified Character subclasses

--- DemoMan.java
+++ DemoMan.java
@@ -2,10 +2,20 @@ public class DemoMan extends Character {
     public DemoMan() {
 	super();
 	hp = 300;
-	
+	speed = 15;
+	damage = 100;
+	luck = 60;
+
+	money = 0;
+
+	normalstats[0] = hp;
+	normalstats[1] = speed;
+	normalstats[2] = damage;
+	normalstats[3] = luck;
     }
 
     public DemoMan(String name) {
-	super(name);
+	this()
+	this.name = name;
     }
 }
