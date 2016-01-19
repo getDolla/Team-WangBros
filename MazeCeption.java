ca5058df494bf7b0283d58befffdf049fc522175
Richard <rwang4@stuy.edu>
2016-01-19 09:50:20 -0500

    MazeCeption

--- MazeCeption.java
+++ MazeCeption.java
@@ -1,25 +1,68 @@
 import java.util.Scanner; 
 
 public class MazeCeption {
-	protected int stage = 0;
+    protected int stage = 0;
+    
+    public MazeCeption() {
+	newgame();
+    }
+    
+    public void newgame() {
+	System.out.println( "Welcome to MazeCeption!" );
+	
+	System.out.println( "Choose your character:" );
 
-	public MazeCeption() {
-		newgame();
+	System.out.println( "Tank" );
+	System.out.println( "Ninja" );
+	System.out.println( "DemoMan" );
+	System.out.println( "DankMeme" );
+	
+	
+	System.out.println( );       
+
+	boolean chosenClass = false;
+	
+	while (!chosenClass) {
+
+	    Scanner in = new Scanner( System.in );
+	    
+	    String choice = in.nextLine();
+	    
+	    if ( choice.toUpperCase().equals( "TANK" ) ) {
+		Engine.character = new Tank();
+		chosenClass = true;
+	    } 
+	    
+	    else if ( choice.toUpperCase().equals( "NINJA" ) ) {
+		Engine.character = new Ninja();
+		chosenClass = true;
+	    }
+	    
+	    else if ( choice.toUpperCase().equals( "DEMOMAN" ) ) {
+		Engine.character = new DemoMan();
+		chosenClass = true;
+	    }
+	    
+	    else if ( choice.toUpperCase().equals( "DANKMEME" ) ) {
+		Engine.character = new DankMeme();
+		chosenClass = true;
+	    }
+	    
+	    Engine.fillMap();
+
+	    
 	}
-
-	public void newgame() {
-		System.out.println( "Welcome to MazeCeption!" );
-
-		System.out.println( "Choose your character:" );
-		
-		System.out.println( "Tank:" );
-		(new Tank()).displaystats();
-
-		System.out.println( "" );
-		Engine.fillMap();
+    }
+	
+    public void play() {
+	while (!Engine.character.inBattle) {
+	    Engine.move();
 	}
-
-	public static void main( String[] args ) {
-		MazeCeption m = new MazeCeption();
-	}
+    }
+    
+    public static void main( String[] args ) {
+	MazeCeption m = new MazeCeption();
+	m.play();
+	
+    }
 }
